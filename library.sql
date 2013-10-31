2c101502969615005b74439c34436ea8a0bab564
John Tanner <johntanner@gmail.com>
2013-10-31 15:42:56 -0700

    Everything except Modifies table works in isolation.
    We need some discussion about the proposed logic.

--- library.sql
+++ library.sql
@@ -1,25 +1,25 @@
-drop table Members
-drop table Librarians
-drop table Branch
-drop table Rental_Due_On
-drop table Book_Copy
-drop table Renews
-drop table Makes_Reservation_or_Rental
-drop table Modifies
-drop table Has_Books
-drop table Time_Period
-drop table Reservation_For
-drop table Reserved_On
+drop table Members CASCADE CONSTRAINTS;
+drop table Librarians CASCADE CONSTRAINTS;
+drop table Branch CASCADE CONSTRAINTS;
+drop table Rental_Due_On CASCADE CONSTRAINTS;
+drop table Book_Copy CASCADE CONSTRAINTS;
+drop table Renews CASCADE CONSTRAINTS;
+drop table Makes_Reservation_Or_Rental CASCADE CONSTRAINTS;
+drop table Modifies CASCADE CONSTRAINTS;
+drop table Has_Books CASCADE CONSTRAINTS;
+drop table Time_Period CASCADE CONSTRAINTS;
+drop table Reservation_For CASCADE CONSTRAINTS;
+drop table Reserved_On CASCADE CONSTRAINTS;
 
 
 CREATE TABLE Members 
 (
-member_id 		CHAR(8),
-address 		VARCHAR(50),
-first_name 		VARCHAR (10),
-last_name 		VARCHAR (10),
-phone_number 	CHAR(10),
-owing	 		REAL,
+member_id CHAR(8),
+address VARCHAR(50),
+first_name VARCHAR(10),
+last_name VARCHAR(10),
+phone_number CHAR(10),
+owing REAL,
 PRIMARY KEY (member_id)
 );
 
@@ -37,30 +37,30 @@ values('00000005', '555 Beta Drive Vancouver BC, V5Y 1S5', 'Ellie', 'Everett', '
 
 CREATE TABLE Librarians
 (
-member_id 		CHAR(8),
-employee_id 		CHAR(8),
-PRIMARY KEY (employee_id , member_id),
-FOREIGN KEY (member_id) REFERENCES Members
+member_id CHAR(8),
+employee_id CHAR(8),
+PRIMARY KEY (member_id, employee_id),
+FOREIGN KEY (member_id) REFERENCES Members ON DELETE CASCADE
 );
 
 insert into Librarians
-values('10000001', '10000009');
+values('00000001', '10000001');
 insert into Librarians
-values('10000002', '10000008');
+values('00000002', '10000002');
 insert into Librarians
-values('10000003', '10000007');
+values('00000003', '10000003');
 insert into Librarians
-values('10000004', '10000006');
+values('00000004', '10000004');
 insert into Librarians
-values('10000005', '10000000');
+values('00000005', '10000005');
 
 
 CREATE TABLE Branch
 (    
-branch_id 		CHAR(4),
-name       		CHAR(30),
-city          		CHAR(20),
-province  		CHAR(5),
+branch_id CHAR(4),
+name CHAR(30),
+city CHAR(20),
+province CHAR(5),
 PRIMARY KEY (branch_id)
 );
 
@@ -76,124 +76,34 @@ insert into Branch
 values('0005', 'Coliseum Public Library', 'Vancouver', 'BC');
 
 
-CREATE TABLE Rental_Due_On
-(	
-	copy_id		CHAR(4),
-	start_date		DATETIME,
-	end_date		DATETIME,
-	rental_id		CHAR(10) not null,
-PRIMARY KEY (rental_id),
-FOREIGN KEY (start_date, end_date) REFERENCES Time_Period,
-FOREIGN KEY (copy_id) REFERENCES Book_Copy
-);
-
-insert into Rental_Due_On
-values('0001', 2013-10-18 13:42:24, 2013-11-01 13:42:24, '0000000001');
-insert into Rental_Due_On
-values('0002', 2013-10-18 08:42:00, 2013-11-01 08:42:00, '0000000002');
-insert into Rental_Due_On
-values('1003', 2013-10-19 09:42:24, 2013-11-02 09:42:24, '0000000003');
-insert into Rental_Due_On
-values('1004', 2013-10-20 12:42:24, 2013-11-03 12:42:24, '0000000004');
-insert into Rental_Due_On
-values('1005', 2013-10-21 00:42:24, 2013-10-04 00:42:24, '0000000005');
-
-
-CREATE TABLE Book_Copy
+CREATE TABLE Time_Period
 (
-copy_id		CHAR(4),
-isbn            		CHAR(13)
-PRIMARY KEY (copy_id, isbn),
-FOREIGN KEY (isbn) REFERENCES Books
+start_date DATE,
+end_date DATE,
+PRIMARY KEY (start_date, end_date)
 );
 
-insert into Book_Copy
-values('0001', '9780672327568');
-insert into Book_Copy
-values('0002', '9780672327568');
-insert into Book_Copy
-values('0001', '9780672327432');
-insert into Book_Copy
-values('0004', '9780672327231');
-insert into Book_Copy
-values('0012', '9780672327495');
-
-
-CREATE TABLE Renews
-(	
-	member_id	 	CHAR(8)
-	rental_id  		CHAR(10)
-	PRIMARY KEY (member_id, rental_id), 
-	FOREIGN KEY (member_id) REFERENCES Members
-	FOREIGN KEY (transaction_id) REFERENCES Rental_Due_On
-);
-
-insert into Renews
-values('00000001', '0000000001');
-insert into Renews
-values('10000002', '0000000002');
-insert into Renews
-values('00000003', '0000000003');
-insert into Renews
-values('00000004', '0000000004');
-insert into Renews
-values('00000005', '0000000005');
-
-
-CREATE TABLE Makes_Reservation_or_Rental
-(
-	member_id 		CHAR(8),
-	rental_id		CHAR(10),
-	reservation_id	CHAR(10),
-	PRIMARY KEY (member_id, rental_id, reservation_id),
-	FOREIGN KEY (member_id) REFERENCES Members,
-	FOREIGN KEY (rental_id) REFERENCES Rental_Due_On,
-	FOREIGN KEY (reservation_id) REFERENCES Reservation_For
-);
-
-insert into Makes_Reservation_or_Rental
-values('00000001', '0000000001', '0000000001');
-insert into Makes_Reservation_or_Rental
-values('00000002', '0000000002', '0000000002');
-insert into Makes_Reservation_or_Rental
-values('00000003', '0000000003', '0000000003');
-insert into Makes_Reservation_or_Rental
-values('00000004', '0000000004', '0000000004');
-insert into Makes_Reservation_or_Rental
-values('00000005', '0000000005', '0000000005');
-
-
-CREATE TABLE Modifies
-(
-	employee_id 		CHAR(8)
-	isbn			CHAR(13)
-	PRIMARY KEY (employee_id, isbn)
-	FOREIGN KEY (employee_id) REFERENCES Librarians
-	FOREIGN KEY (isbn) REFERENCES Books
-);
-
-insert into Modifies
-values('10000001', '9780672327432');
-insert into Modifies
-values('10000002', '9780672327564');
-insert into Modifies
-values('10000003', '9780672327245');
-insert into Modifies
-values('10000004', '9780672327436');
-insert into Modifies
-values('10000005', '9780672327231');
+insert into Time_Period
+values('19-Oct-13', '01-Nov-13');
+insert into Time_Period
+values('20-Oct-13', '02-Nov-13');
+insert into Time_Period
+values('21-Oct-13', '03-Nov-13');
+insert into Time_Period
+values('22-Oct-13', '04-Nov-13');
+insert into Time_Period
+values('23-Oct-13', '05-Nov-13');
 
 
 CREATE TABLE Has_Books 
 (
-isbn 			CHAR(13),
-publisher 		CHAR(35),
-title	    		CHAR(40),
-author	    		CHAR(20),
-branch_id 		CHAR(4) NOT NULL,
+isbn CHAR(13),
+publisher CHAR(35),
+title CHAR(40),
+author CHAR(20),
+branch_id CHAR(4) NOT NULL,
 PRIMARY KEY (isbn),
-FOREIGN KEY (branch_id) REFERENCES Branch,
-ON DELETE CASCADE
+FOREIGN KEY (branch_id) REFERENCES Branch ON DELETE CASCADE
 );
 
 insert into Has_Books
@@ -205,65 +115,156 @@ values('9780672327454', 'Carrot Publishing', 'How To Win Or Lose', 'Loretta Loui
 insert into Has_Books
 values('9780672327243', 'Durian Publishing', 'Winning Is Not Everything', 'Marlene Mayall', '0004');
 insert into Has_Books
-values('9780672327432', 'Eggplant Publishing', 'Winning Really Is Everything', 'Nigella Ness', '0005');
+values('9780672327433', 'Eggplant Publishing', 'Winning Really Is Everything', 'Nigella Ness', '0005');
 
 
-CREATE TABLE Time_Period
+CREATE TABLE Book_Copy
 (
-	start_date		DATETIME
-	end_date		DATETIME
-	PRIMARY KEY (start_date, end_date)
+copy_id CHAR(4),
+isbn CHAR(13),
+PRIMARY KEY (copy_id, isbn),
+FOREIGN KEY (isbn) REFERENCES Has_Books ON DELETE CASCADE
 );
 
-insert into Time_Period
-values(2013-10-19 09:42:25, 2013-11-01 09:42:25);
-insert into Time_Period
-values(2013-10-19 09:42:26, 2013-11-01 09:42:26);
-insert into Time_Period
-values(2013-10-19 09:42:26, 2013-11-01 09:42:26);
-insert into Time_Period
-values(2013-10-19 09:42:27, 2013-11-01 09:42:27);
-insert into Time_Period
-values(2013-10-19 09:42:28, 2013-11-01 09:42:28);
+insert into Book_Copy
+values('0001', '9780672327432');
+insert into Book_Copy
+values('0002', '9780672327231');
+insert into Book_Copy
+values('0001', '9780672327454');
+insert into Book_Copy
+values('0004', '9780672327243');
+insert into Book_Copy
+values('0012', '9780672327433');
+
+
+CREATE TABLE Rental_Due_On
+(	
+copy_id CHAR(4),
+isbn CHAR(13),
+start_date DATE,
+end_date DATE,
+rental_id CHAR(10) NOT NULL,
+PRIMARY KEY (rental_id),
+FOREIGN KEY (start_date, end_date) REFERENCES Time_Period ON DELETE CASCADE,
+FOREIGN KEY (copy_id, isbn) REFERENCES Book_Copy ON DELETE CASCADE
+);
+
+insert into Rental_Due_On
+values('0001', '9780672327432', '19-Oct-13', '01-Nov-13', '0000000001');
+insert into Rental_Due_On
+values('0002', '9780672327231', '20-Oct-13', '02-Nov-13', '0000000002');
+insert into Rental_Due_On
+values('0001', '9780672327454', '21-Oct-13', '03-Nov-13', '0000000003');
+insert into Rental_Due_On
+values('0004', '9780672327243', '22-Oct-13', '04-Nov-13', '0000000004');
+insert into Rental_Due_On
+values('0012', '9780672327433', '23-Oct-13', '05-Nov-13', '0000000005');
+
+
+CREATE TABLE Renews
+(	
+member_id CHAR(8),
+rental_id CHAR(10),
+PRIMARY KEY (member_id, rental_id), 
+FOREIGN KEY (member_id) REFERENCES Members ON DELETE CASCADE,
+FOREIGN KEY (rental_id) REFERENCES Rental_Due_On ON DELETE CASCADE
+);
+
+insert into Renews
+values('00000001', '0000000001');
+insert into Renews
+values('00000002', '0000000002');
+insert into Renews
+values('00000003', '0000000003');
+insert into Renews
+values('00000004', '0000000004');
+insert into Renews
+values('00000005', '0000000005');
 
 
 CREATE TABLE Reservation_For
 (
-reservation_id		CHAR(10)
-isbn			CHAR(13)
-PRIMARY KEY (reservation_id)
-FOREIGN KEY (isbn) REFERENCES Books
+reservation_id CHAR(10),
+isbn CHAR(13),
+PRIMARY KEY (reservation_id),
+FOREIGN KEY (isbn) REFERENCES Has_Books
 );
 
-insert into Reserved_For
+insert into Reservation_For
 values('0000000001', '9780672327432');
-insert into Reserved_For
-values('0000000002', '9780672327475');
-insert into Reserved_For
-values('0000000003', '9780672327456');
-insert into Reserved_For
-values('0000000004', '9780672327443');
-insert into Reserved_For
-values('0000000005', '9780672327425');
+insert into Reservation_For
+values('0000000002', '9780672327231');
+insert into Reservation_For
+values('0000000003', '9780672327454');
+insert into Reservation_For
+values('0000000004', '9780672327243');
+insert into Reservation_For
+values('0000000005', '9780672327433');
+
+
+CREATE TABLE Makes_Reservation_Or_Rental
+(
+member_id CHAR(8),
+rental_id CHAR(10),
+reservation_id CHAR(10),
+PRIMARY KEY (member_id, rental_id, reservation_id),
+FOREIGN KEY (member_id) REFERENCES Members ON DELETE CASCADE,
+FOREIGN KEY (rental_id) REFERENCES Rental_Due_On ON DELETE CASCADE,
+FOREIGN KEY (reservation_id) REFERENCES Reservation_For ON DELETE CASCADE
+);
+
+insert into Makes_Reservation_Or_Rental
+values('00000001', '0000000001', '0000000001');
+insert into Makes_Reservation_Or_Rental
+values('00000002', '0000000002', '0000000002');
+insert into Makes_Reservation_Or_Rental
+values('00000003', '0000000003', '0000000003');
+insert into Makes_Reservation_Or_Rental
+values('00000004', '0000000004', '0000000004');
+insert into Makes_Reservation_Or_Rental
+values('00000005', '0000000005', '0000000005');
+
+
+CREATE TABLE Modifies
+(
+member_id CHAR(8),
+employee_id CHAR(8),
+isbn CHAR(13),
+PRIMARY KEY (member_id, employee_id),
+FOREIGN KEY (member_id, employee_id) REFERENCES Librarians ON DELETE CASCADE,
+FOREIGN KEY (isbn) REFERENCES Has_Books ON DELETE CASCADE
+);
+
+insert into Modifies
+values('00000001', '10000001', '9780672327432');
+insert into Modifies
+values('00000002', '10000002', '9780672327564');
+insert into Modifies
+values('00000003', '10000003', '9780672327245');
+insert into Modifies
+values('00000004', '10000004', '9780672327436');
+insert into Modifies
+values('00000005', '10000005', '9780672327231');
 
 
 CREATE TABLE Reserved_On
 (	
-	member_id		CHAR(8)
-	start_date		DATETIME
-	end_date		DATETIME
-	PRIMARY KEY (start_date, end_date, member_id)
-	FOREIGN KEY (member_id) REFERENCES Members
-	FOREIGN KEY (start_date, end_date) REFERENCES Time_Period
+member_id CHAR(8),
+start_date DATE,
+end_date DATE,
+PRIMARY KEY (start_date, end_date, member_id),
+FOREIGN KEY (member_id) REFERENCES Members ON DELETE CASCADE,
+FOREIGN KEY (start_date, end_date) REFERENCES Time_Period ON DELETE CASCADE
 );
 
 insert into Reserved_On
-values('00000001', 2013-10-19 09:42:26, 2013-10-22 09:42:26);
+values('00000001', '19-Oct-13', '01-Nov-13');
 insert into Reserved_On
-values('00000002', 2013-10-20 09:42:26, 2013-10-23 09:42:26);
+values('00000002', '20-Oct-13', '02-Nov-13');
 insert into Reserved_On
-values('00000003', 2013-10-21 09:42:26, 2013-10-24 09:42:26);
+values('00000003', '21-Oct-13', '03-Nov-13');
 insert into Reserved_On
-values('00000004', 2013-10-22 09:42:26, 2013-10-25 09:42:26);
+values('00000004', '22-Oct-13', '04-Nov-13');
 insert into Reserved_On
-values('00000005', 2013-10-23 09:42:26, 2013-10-26 09:42:26);
+values('00000005', '23-Oct-13', '05-Nov-13');
